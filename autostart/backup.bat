190caf592d7e4d43369b0b1e68953981ed929775
Timothy Do <timothydobsa@gmail.com>
2021-04-17 19:25:08 -0700

    Update backup.bat

--- autostart/backup.bat
+++ autostart/backup.bat
@@ -3,4 +3,8 @@ rmdir "Timothy's UCI Dorm" /s /q
 mkdir "Timothy's UCI Dorm"
 cd "Timothy's UCI Dorm"
 xcopy C:\Users\SERVER-TD2020\Desktop\"BobTD Minecraft Server"\"Bedrock 1.16.220.02 - Timothy's UCI Dorm"\worlds\"Timothy's UCI Dorm" /e
+cd ../..
+git add * 
+git commit -m "Backup Autoscript"
+git push
 
