8e20f1266eb697535e0787a93dcd0b918f1598c0
Timothy Do <timothydobsa@gmail.com>
2021-04-17 19:20:17 -0700

    created backup.bat

--- autostart/backup.bat
+++ autostart/backup.bat
@@ -1,1 +1,6 @@
-copy C:\Users\SERVER-TD2020\Desktop\"BobTD Minecraft Server"\"Bedrock 1.16.220.02 - Timothy's UCI Dorm"\worlds\"Timothy's UCI Dorm" .
+cd ../Worlds
+rmdir "Timothy's UCI Dorm" /s /q
+mkdir "Timothy's UCI Dorm"
+cd "Timothy's UCI Dorm"
+xcopy C:\Users\SERVER-TD2020\Desktop\"BobTD Minecraft Server"\"Bedrock 1.16.220.02 - Timothy's UCI Dorm"\worlds\"Timothy's UCI Dorm" /e
+
