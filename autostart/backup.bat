43f2c461ef3eec482db403b836781fb155bae14d
Timothy Do <timothydobsa@gmail.com>
2021-06-12 20:24:18 -0700

    Update 1.17

--- autostart/backup.bat
+++ autostart/backup.bat
@@ -2,7 +2,7 @@ cd ../Worlds
 rmdir "Timothy's UCI Dorm" /s /q
 mkdir "Timothy's UCI Dorm"
 cd "Timothy's UCI Dorm"
-xcopy C:\Users\SERVER-TD2020\Desktop\"BobTD Minecraft Server"\"Bedrock 1.16.220.02 - Timothy's UCI Dorm"\worlds\"Timothy's UCI Dorm" /e
+xcopy C:\Users\SERVER-TD2020\Desktop\"BobTD Minecraft Server"\"Bedrock 1.17 - Timothy's UCI Dorm"\worlds\"Timothy's UCI Dorm" /e
 cd ../..
 python commit.py
 
