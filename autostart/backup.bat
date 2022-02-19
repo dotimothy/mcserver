7c37f1a2cdb519540887509c35e723ca5dfbf55b
Timothy Do <timothydobsa@gmail.com>
2022-02-19 11:17:15 -0800

    Auto-Commit mcserver on 02/19/22

--- autostart/backup.bat
+++ autostart/backup.bat
@@ -2,7 +2,7 @@ cd ../Worlds
 rmdir "Timothy's UCI Dorm" /s /q
 mkdir "Timothy's UCI Dorm"
 cd "Timothy's UCI Dorm"
-xcopy C:\Users\SERVER-TD2020\Desktop\"BobTD Minecraft Server"\"Bedrock 1.18.2.03 - Timothy's UCI Dorm"\worlds\"Timothy's UCI Dorm" /e
+xcopy C:\Users\SERVER-TD2020\Desktop\"BobTD Minecraft Server"\"Bedrock 1.18.12.01 - Timothy's UCI Dorm"\worlds\"Timothy's UCI Dorm" /e
 cd ../..
 python commit.py
 cd autostart
