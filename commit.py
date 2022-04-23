202d113774ac58e16eee5f50243937fc8302f350
Timothy Do <timothydobsa@gmail.com>
2022-04-23 15:37:02 -0700

    Auto-Commit mcserver on 04/23/22

--- commit.py
+++ commit.py
@@ -6,6 +6,7 @@ date = datetime.datetime.now()
 commit = "Auto-Commit mcserver on " + date.strftime("%x")
 
 #git commits.
+os.system("git pull")
 os.system("git add *")
 os.system("git commit -m \"" + commit + "\"")
 os.system("git push")
