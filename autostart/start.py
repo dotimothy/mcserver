faaff3dbfd6858d62d7100f0e7f0fd0aee817c2f
Timothy Do <timothydobsa@gmail.com>
2023-10-15 12:06:29 -0700

    Auto-Commit mcserver on 10/15/23

--- autostart/start.py
+++ autostart/start.py
@@ -6,5 +6,5 @@ year = date.year
 month = date.month
 day = date.day
 file = str(month) + "-" + str(day) + "-" + str(year) + ".txt"
-version = "1.19.21.01"
+version = "1.20.32.03"
 os.system("C:\\Users\\SERVER-TD2020\\Desktop\\\"BobTD Minecraft Server\"\\\"Bedrock " + version + " - Timothy's UCI Dorm\"\\bedrock_server.exe >> ..\\logs\\" + file)
